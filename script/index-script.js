046346705b482630acebaf322e64e43dd80e4b7f
yashsehgal <yashsehgal.work@gmail.com>
2020-07-30 21:17:44 +0530

    updated codebase

--- script/index-script.js
+++ script/index-script.js
@@ -3,5 +3,6 @@ const hosting_port = "63342"
 const server_out_url = "http://localhost:63342/rock-paper-scissors-game-project/index.htm?_ijt=skbra2v82g45850pep5ksn836k"
 
 function rock_selection() {
-    console.log("rock is selected by the user")
+    // console.log("rock is selected by the user")
+
 }
